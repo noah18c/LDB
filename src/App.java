e3b97adc3d819eb4cdb4d40e5f33f7e33520c4a0
noah18c <noahcroes@gmail.com>
2021-10-06 13:51:48 +0200

    hoi

--- src/App.java
+++ src/App.java
@@ -5,11 +5,15 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class App {
+public class App extends SQLException {
     public static void main(String[] args) throws SQLException{
         String url = "jdbc:mysql://localhost:3306/ldb";
         String user = "root";
         String password = "Aftershave1808";
+
+        /*
+        IntroScreen intro = new IntroScreen();
+        intro.run();
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -17,20 +21,44 @@ public class App {
             e.printStackTrace();
         }
 
+        Connection con = null;
+
         try {
-            Connection con = DriverManager.getConnection(url, user, password);
+            con = DriverManager.getConnection(url, user, password);
             Statement st = con.createStatement();
             ResultSet rs = st.executeQuery("Select pizza_name From pizza");
-            String data = "";
-            int i = 1;
-            while(rs.next()) {
-                data+=rs.getString(i);
-            }
-            System.out.println(data);
-
             con.close();
         } catch (SQLException e) {
-            System.out.println(e);
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+
         }
+
+
+
+
+        String query = "SELECT * FROM pizza";
+        ResultSet rs = null;
+
+
+        try (Statement stmt = con.createStatement()) {
+            rs = stmt.executeQuery(query);
+            while(rs.next()){
+                System.out.println(rs.getString("pizza_name"));
+            }
+        } catch (SQLException e){
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        } finally {
+            if(rs != null){
+
+            }
+        }
+        */
+
+
+
     }
 }
---
+++ src/IntroScreen.java
@@ -0,0 +1,16 @@
+public class IntroScreen {
+
+    public IntroScreen(){
+
+    }
+
+
+    public void run() {
+        System.out.println("Welcome -- Herzlich Wilkommen -- Sex?");
+
+    }
+
+    private void welcomeMessage() {
+
+    }
+}
